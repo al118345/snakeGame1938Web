e1bc5b156231ddeff8273c7922539ecb9b524ca6
ruben <rubenpeib@gmail.com>
2022-05-24 17:32:44 +0200

    añado la 3 parte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {Food} from "./game-engine/food";
 import {Snake} from "./game-engine/snake";
+import {outsideGrid} from "./game-engine/gameboard-grid.util";
 
 @Component({
   selector: 'app-root',
@@ -13,17 +14,34 @@ export class AppComponent  implements OnInit, AfterViewInit {
   snake = new Snake();
   food = new Food(this.snake);
 
+
+  lastRenderTime = 0
+  gameOver = false
+
   ngAfterViewInit() {
     this.gameBoard = document.querySelector('.game-board');
     window.requestAnimationFrame(this.start.bind(this));
   }
 
   ngOnInit(): void {
+    this.snake.listenToInputs();
+  }
+  dpadMovement(direction: string) {
+    this.snake.input.setDirection(direction);
   }
 
+
   start(currentTime: any) {
+    if (this.gameOver) {
+      return console.log('Game Over');
+    }
 
     window.requestAnimationFrame(this.start.bind(this));
+    const secondsSinceLastRender = (currentTime - this.lastRenderTime) / 1000;
+    if (secondsSinceLastRender < 1 / this.snakeSpeed) {
+      return;
+    }
+    this.lastRenderTime = currentTime;
 
     this.update();
     this.draw();
@@ -39,4 +57,28 @@ export class AppComponent  implements OnInit, AfterViewInit {
     this.snake.draw(this.gameBoard);
     this.food.draw(this.gameBoard);
   }
+
+  checkDeath() {
+    this.gameOver = outsideGrid(this.snake.getSnakeHead()) || this.snake.snakeIntersection();
+    if (!this.gameOver) {
+      return;
+    }
+    this.gameBoard.classList.add('blur');
+  }
+
+
+  get snakeSpeed() {
+    const score = this.food.currentScore;
+    if (score < 10) {
+      return 4;
+    }
+    if (score > 10 && score < 15) {
+      return 5;
+    }
+    if (score > 15 && score < 20) {
+      return 6;
+    }
+    return 7;
+  }
+
 }
